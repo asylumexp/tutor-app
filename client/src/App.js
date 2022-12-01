ce1e552cda03b655eccf7bc4eb5bb457402a5a30
Sam Heinz <sam@samheinz.com>
2022-12-01 13:25:20 +1000

    kablam

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  const [backendData, setBackendData] = useState([{}])
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
