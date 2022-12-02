6349d965970212fdfcf5820c0c4e14dd2ba02503
Sam Heinz <sam@samheinz.com>
2022-12-02 11:54:58 +1000

    swift placeholder
    Signed-off-by: Sam Heinz <sam@samheinz.com>

---
+++ swift/tutorApp/tutorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorAppApp.swift
+//  tutorApp
+//
+//  Created by Samuel Heinz on 2/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct tutorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
