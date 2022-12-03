8f788adadb17aa34241c2237c485b75e66e4b89c
Sam Heinz <sam@samheinz.com>
2022-12-03 19:31:40 +1000

    pov im me ur you (fixing dependabot)

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pugs');
 
 app.use(cors());
 app.use(logger('dev'));
