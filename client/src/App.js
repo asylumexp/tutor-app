9bc73f86cfcffe58aac69acda9eed1ca27edd42c
Sam Heinz <sam@samheinz.com>
2022-12-04 21:30:16 +1000

    prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
- function App() {
-   const [backendData, setBackendData] = useState([{}])
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
 
-   useEffect(() => {
-     fetch("http://localhost:9000/testapi").then(
-       response => response.json()
-     ).then(
-       data => {
-         setBackendData(data)
-       }
-     )
-   }, [])
+  useEffect(() => {
+    fetch("http://localhost:9000/testapi")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
-   return (
-     <div>
-          {console.log(backendData)}
-       {(typeof backendData.user === 'undefined') ? (
-         <p>Loading...</p>
-       ): (
-         backendData.user.map((user, i) => (
-           <p key={i}>{user}</p>
-         ))
-       )}
-     </div>
-   )
- }
+  return (
+    <div>
+      {typeof backendData.user === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.user.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+  );
+}
 
- export default App
- 
+export default App;
