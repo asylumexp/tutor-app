f912c774238445eec2c12be702494bfb0ce0c24c
Will Coggins <110357970+srcoggin@users.noreply.github.com>
2022-12-06 19:01:34 +1000

    Updated App, Just fiddling with cosmetic functions, i need an update on what the next step is. i recon we should fopcus on how we are tutoring. using modules? classes? like how are we doing this. the diagram doesn say much

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const logo = require("./assets/tutor-app-logos_transparent_alt.png");
 
   useEffect(() => {
+    // ? can we please make like like "localhost:9000/BingChilling" omg that would be so cool
     fetch("http://localhost:9000/testapi")
       .then((response) => response.json())
       .then((data) => {
@@ -22,13 +23,13 @@ function App() {
           position: "fixed",
           left: "12.5%",
           width: "calc(100% - 12.5%)",
-          height: "50px",
+          height: "60px",
         }}
       >
         <Nav>
-          <Nav.Link href="http://example.com">Home</Nav.Link>
-          <Nav.Link href="http://example.com/orders">Orders</Nav.Link>
-          <Nav.Link href="http://example.com/settings">Settings</Nav.Link>
+          <Nav.Link href="http://BingChilling.com">Home</Nav.Link>
+          <Nav.Link href="http://BingChilling/Orders">Orders</Nav.Link>
+          <Nav.Link href="http://BingChilling.com/Settings">Settings</Nav.Link>
         </Nav>
       </Navbar>
       <Container>
@@ -36,22 +37,22 @@ function App() {
           <Col
             bg="dark"
             varient="dark"
-            width="50px"
+            width="60px"
             style={{
-              backgroundColor: "#212529",
+              backgroundColor: "#33393E",
               position: "fixed",
               left: "0",
-              width: "12.5%",
+              width: "14.5%",
               height: "100%",
             }}
           >
             <img
               src={logo}
               alt="Sidebar title"
-              style={{ width: "100%", height: "50px" }}
+              style={{ width: "100%", height: "60px" }}
             />
             {typeof backendData.user === "undefined" ? (
-              <p style={{ color: "white" }}>Loading...</p>
+              <p style={{ color: "white" }}>LOADING...</p>
             ) : (
               backendData.user.map((user, i) => (
                 <p
