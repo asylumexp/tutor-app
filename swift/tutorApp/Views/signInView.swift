232869398d59dfdb8774e68c92ac1f072d4325bc
Sam Heinz <sam@samheinz.com>
2022-12-15 00:17:41 +1000

    rudimentary login UI made (swift)

--- swift/tutorApp/Views/signInView.swift
+++ swift/tutorApp/Views/signInView.swift
@@ -9,21 +9,39 @@ import SwiftUI
 
 struct signInView: View {
     @State var parentVar: Binding<String>
+    @State var text = "asd"
     init(parentVar: Binding<String>) {
         self.parentVar = parentVar
     }
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.init(hex: "071B33"), .init(hex: "833F46"), .init(hex: "FFB123")]), startPoint: .topTrailing, endPoint: .bottomLeading)
-                .ignoresSafeArea(edges: .all)
-            VStack {
-                Button(action: {
-                    print("MyNewPrimitiveButton triggered. Is it printed ?")
-                }){ Text("amazing app").padding() }
-                    .buttonStyle(MyNewPrimitiveButtonStyle(color: .yellow, parentChange: parentVar))
-                
+        HStack {
+            
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.init(hex: "071B33"), .init(hex: "833F46"), .init(hex: "FFB123")]), startPoint: .topTrailing, endPoint: .bottomLeading)
+                    .ignoresSafeArea(edges: .all)
+                TextField("Placeholder", text: $text)
+                    .padding(.horizontal, 50.0)
+                    .textFieldStyle(OutlinedTextFieldStyle(icon: Image(systemName: "lock")))
+                    .offset(y:-240)
+                SecureField("Placeholder", text: $text)
+                    .padding(.horizontal, 50.0)
+                    .textFieldStyle(OutlinedTextFieldStyle(icon: Image(systemName: "lock")))
+                    .offset(y:-120)
+                VStack {
+                    Button(action: {
+                        print("MyNewPrimitiveButton triggered. Is it printed ?")
+                    }){ Text("amazing app").padding() }
+                        .buttonStyle(MyNewPrimitiveButtonStyle(color: .yellow, parentChange: parentVar))
+                    
+                }
             }
         }
+    }
+}
+
+struct signInView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
