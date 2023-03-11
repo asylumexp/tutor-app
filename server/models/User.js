6e17469f6d624625bc2d686d570ddfca203c6a24
Charl1e-D <dovec1@shalomcollege.com>
2023-03-11 20:32:37 +1000

    Update UserModel - backend
    my life is misery

--- server/models/User.js
+++ server/models/User.js
@@ -1,84 +1,71 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        require: true,
-        min: 3,
-        max: 20,
-        unique: true,
-
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
     },
 
-    email:{
-        type: String,
-        required: true,
-        max:50,
-        unique: true, 
-
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
     },
 
-    password:{
-        type: String,
-        required: true,
-        min:6,
-
+    password: {
+      type: String,
+      required: true,
+      min: 6,
     },
 
-    profilePicture:{
-        type:String,
-        default: "",
-
+    profilePicture: {
+      type: String,
+      default: "",
     },
 
-    coverPicture:{
-        type:String,
-        default: "",
-
+    isTutor: {
+      type: Boolean,
+      default: false,
     },
 
-    followers:{
-        type:Array,
-        default:[],
-
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
 
-    followings:{
-        type:Array,
-        default:[],
-
+    tutors: {
+      type: Array,
+      default: [],
     },
 
-    isAdmin:{
-        type:Boolean,
-        default: false,
-
+    students: {
+      type: Array,
+      default: [],
     },
+
     //description
-    desc:{
-        type:String,
-        max: 50,
+    desc: {
+      type: String,
+      max: 50,
     },
 
-    city:{
-        type:String,
-        max: 50,
+    city: {
+      type: String,
+      max: 50,
     },
 
-    from:{
-        type:String,
-        max: 50,
+    interestTopics: {
+      type: Array,
+      default: ["All"],
     },
+  },
 
-    relationship:{
-        type:Number,
-        enum:[1,2,3],
-    },
-
-},
-
-{timestamps: true}
-
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
