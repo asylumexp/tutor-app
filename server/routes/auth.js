661e710f0f6742cf3f1980d398c1241fd2fc718c
Charl1e-D <dovec1@shalomcollege.com>
2023-03-11 22:27:23 +1000

    Only return UserId at login/register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,47 +4,45 @@ const bcrypt = require("bcryptjs");
 
 //REGISTER
 router.post("/register", async (req, res) => {
-    try {
-        //generate new password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  try {
+    console.log("hi");
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        //create new user
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-        });
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-        //save user and respond
-        const user = await newUser.save();
-        res.status(200).json(user);
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json({ userId: user._id });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 //LOGIN
 router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) {
-            res.status(404).json("user not found");
-        }
-
-        else if (await bcrypt.compare(req.body.password, user.password) == false)
-            res.status(400).json("wrong password")
-
-        else {
-            res.status(200).json(user)
-        }
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(404).json("user not found");
+    } else if (
+      (await bcrypt.compare(req.body.password, user.password)) == false
+    )
+      res.status(400).json("wrong password");
+    else {
+      res.status(200).json({ userId: user._id });
     }
-
-    catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
