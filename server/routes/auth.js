e5d6af9a502123b647637940ebbfae56ca584861
Sam Heinz <sam@samheinz.com>
2023-03-22 07:26:55 +1000

    Add more validation checking

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,8 @@ var limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 30,
 });
-var loginValidate = [
+
+const loginValidate = [
   // Check Username
   check("email", ["email", "Username Must Be an Email Address"])
     .isEmail()
@@ -26,9 +27,34 @@ var loginValidate = [
     .trim()
     .escape(),
 ];
+
+const registerValidate = [
+  // Check Username
+  check("email", ["email", "Username Must Be an Email Address"])
+    .isEmail()
+    .trim()
+    .escape()
+    .normalizeEmail(),
+  // Check Password
+  check("username")
+    .isLength({ min: 4 })
+    .withMessage(["user", "Username must be at least 4 characters!"])
+    .trim(' []{}=+_",?)(*&^%$#@!~`|/,<>?:')
+    .escape(),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage(["password", "Password Must Be at Least 8 Characters"])
+    .matches("[0-9]")
+    .withMessage(["password", "Password Must Contain a Number"])
+    .matches("[A-Z]")
+    .withMessage(["password", "Password Must Contain an Uppercase Letter"])
+    .trim()
+    .escape(),
+];
+
 router.use(limiter);
 //REGISTER
-router.post("/register", async (req, res) => {
+router.post("/register", registerValidate, async (req, res) => {
   try {
     console.log("hi");
     //generate new password
@@ -53,16 +79,12 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 router.post("/login", loginValidate, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    const user = await User.findOne({
-      email: req.body.email,
-    });
-    if (!errors.isEmpty()) {
-      res.status(555).json(errors.array());
-    } else {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(555).json(errors.array());
+  } else {
+    try {
       const user = await User.findOne({ email: req.body.email });
-
       if (!user) {
         res.status(404).json("user not found");
       } else if (
@@ -72,10 +94,10 @@ router.post("/login", loginValidate, async (req, res) => {
       else {
         res.status(200).json({ userId: user._id });
       }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
   }
 });
 
