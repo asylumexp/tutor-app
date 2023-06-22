67d18490d9dd2006749f3ce129516d26b8a9fa78
Sam Heinz <54530346+samh06@users.noreply.github.com>
2023-06-22 20:12:30 +1000

    implement saving users to database if they are new

--- lib/login_functions.dart
+++ lib/login_functions.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -10,6 +11,29 @@ Future<String> login() async {
     // Checks that this isnt web, if it isnt them redirect to tutorapp URL
     redirectTo: kIsWeb ? null : 'tutorapp://localhost/',
   );
+  supabase.auth.onAuthStateChange.listen((data) async {
+    final AuthChangeEvent event = data.event;
+    if (event == AuthChangeEvent.signedIn) {
+      sleep(const Duration(seconds: 2));
+      await supabase
+          .from("tutor")
+          .select()
+          .eq("userid", supabase.auth.currentUser!.id)
+          .then((res) async {
+        print(res);
+        if (res.toString() == "[]") {
+          await supabase.from("tutor").insert({
+            "first_name": "Test",
+            "last_name": "test",
+            "email": supabase.auth.currentUser!.email,
+            "subjects": ["None"],
+            "students": ["None"],
+            "userid": supabase.auth.currentUser!.id
+          });
+        }
+      });
+    }
+  });
 
   return '';
 }
